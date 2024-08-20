51810f32e1c96b6eed17232813d739c0e88c729c
Joey Herring <joey@w04h.xyz>
2024-08-20 16:22:52 +1200

    Changes for GitHub pages hosting

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -32,6 +32,7 @@ const siteConfig = {
   // Used for publishing and more
   projectName: 'civis-docs',
   organizationName: 'civisrobotics',
+  trailingSlash: false,
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -73,7 +74,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Civis Robotics`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
